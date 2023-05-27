375f69c2e78e3f586e514c5467f08740aec06bf8
Siddhant Raj <siddhantraj04@gmail.com>
2023-05-27 12:33:40 +0530

    Add win combinations

--- main.js
+++ main.js
@@ -1,31 +1,75 @@
-const gameBoard=document.querySelector('#gameboard');
-const infoDisplay=document.querySelector('#info');
-const startCells=["","","","","","","","",""];
+const gameBoard = document.querySelector('#gameboard');
+const infoDisplay = document.querySelector('#info');
+const startCells = ["", "", "", "", "", "", "", "", ""];
+const cellElements = [];
 
-let turn="circle";
-infoDisplay.textContent=`It is ${turn}'s turn`;
+let turn = "circle";
+infoDisplay.textContent = `It is ${turn}'s turn`;
 
-
-function createGameBoard(){
-    startCells.forEach((cell,index)=>{
-        const cellElement=document.createElement('div');
-        cellElement.classList.add("square");
-        cellElement.id=index;
-        cellElement.addEventListener('click',handleCellClick);
-        gameBoard.append(cellElement);
-    })    
+function createGameBoard() {
+  startCells.forEach((cell, index) => {
+    const cellElement = document.createElement('div');
+    cellElement.classList.add("square");
+    cellElement.id = index;
+    cellElement.addEventListener('click', handleCellClick);
+    gameBoard.append(cellElement);
+    cellElements.push(cellElement);
+  })
 }
 
-createGameBoard();   
+createGameBoard();
 
-function handleCellClick(e){
-    const display=document.createElement('div');
-    display.classList.add(turn);
-    e.target.append(display);
-    if(turn==="circle"){turn="cross";}
-    else{turn="circle";}
-    infoDisplay.textContent=`It is ${turn}'s turn`;
-    e.target.removeEventListener('click',handleCellClick); 
-    checkScore();
+function handleCellClick(e) {
+  const display = document.createElement('div');
+  display.classList.add(turn);
+  e.target.append(display);
+  if (turn === "circle") {
+    turn = "cross";
+  } else {
+    turn = "circle";
+  }
+  infoDisplay.textContent = `It is ${turn}'s turn`;
+  e.target.removeEventListener('click', handleCellClick);
+  checkScore();
 }
 
+function checkScore() {
+  const allSquares = document.querySelectorAll('.square');
+
+  const WinCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ];
+
+  WinCombinations.forEach((combination) => {
+    const circleWins = combination.every((index) => {
+      return allSquares[index].firstChild?.classList.contains('circle');
+    });
+    if (circleWins) {
+      infoDisplay.textContent = "Circle Wins";
+      cellElements.forEach((cellElement) => {
+        cellElement.removeEventListener('click', handleCellClick);
+      });
+      return;
+    }
+  });
+
+  WinCombinations.forEach((combination) => {
+    const crossWins = combination.every((index) => {
+      return allSquares[index].firstChild?.classList.contains('cross');
+    });
+    if (crossWins) {
+      infoDisplay.textContent = "Cross Wins";
+      cellElements.forEach((cellElement) => {
+        cellElement.removeEventListener('click', handleCellClick);
+      });
+      return;
+    }
+  });
+}
